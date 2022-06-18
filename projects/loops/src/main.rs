e77e54378cc87a9d57ec349041d0a3b4ad25899d
Lenny Peng <unisko@gmail.com>
2022-06-18 20:51:21 +0800

    Updated.

--- projects/loops/src/main.rs
+++ projects/loops/src/main.rs
@@ -1,23 +1,13 @@
 fn main() {
-    let mut count = 0;
+    let mut counter = 0;
 
-    'counting_up: loop {
-        println! ("计数 = {}", count);
-        let mut remaining = 10;
+    let result = loop {
+        counter += 1;
 
-        loop {
-            println! ("剩余 = {}", remaining);
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
+        if counter == 10 {
+            break counter * 2;
         }
+    };
 
-        count += 1;
-    }
-
-    println! ("最终计数 = {}", count);
+    println! ("结果为：{}", result);
 }
