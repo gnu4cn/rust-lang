3432f90607072fafa83bd4eb4fc917b21a8b03bf
Lenny Peng <unisko@gmail.com>
2022-06-19 11:22:27 +0800

    Updated.

--- projects/loops/src/main.rs
+++ projects/loops/src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-
-    let mut index = 0;
-
-    while index < a.len() {
-        println! ("值为：{}", a[index]);
-
-        index += 1;
+    for number in (1..4).rev() {
+        println! ("{}!", number);
     }
+
+    println! ("发射！！");
 }
