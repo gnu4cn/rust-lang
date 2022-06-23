1dc41568dc1c15264ca330cf940a8e5f60ea091a
Peng Hailin <unisko@gmail.com>
2022-06-23 17:50:15 +0800

    Updated.

--- projects/ownership_demo/src/main.rs
+++ projects/ownership_demo/src/main.rs
@@ -2,7 +2,10 @@ fn main() {
     let mut s = String::from("hello");
 
     let r1 = &mut s;
+
+    println! ("{}", r1);
+
     let r2 = &mut s;
 
-    println! ("{}, {}", r1, r2);
+    println! ("{}", r2);
 }
