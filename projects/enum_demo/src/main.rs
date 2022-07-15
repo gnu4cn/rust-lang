f035eb090cc22677da5e729079b36a2a1ab45d30
Peng Hailin <unisko@gmail.com>
2022-07-15 17:52:00 +0800

    Updated.

--- projects/enum_demo/src/main.rs
+++ projects/enum_demo/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     let config_max = Some(3u8);
+    let config_none: Option<u8> = None;
 
-    if let Some(max) = config_max {
+    if let None =  config_none {
+        println! ("极大值未被设置");
+    }
+
+    if let Some(max) =  config_max {
         println! ("极大值被设置为了 {}", max);
     }
 }
