a1a5449945c22be2a6f646c8efc06f8352c7d3fd
Peng Hailin <unisko@gmail.com>
2022-07-25 18:01:57 +0800

    Updated.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,10 +1,24 @@
-fn deliver_order() {}
-
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    fn cook_order() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // 点下一份带有黑麦土司的夏日早餐
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println! ("请给我一份 {} 土司", meal.toast);
+
+    // 
 }
